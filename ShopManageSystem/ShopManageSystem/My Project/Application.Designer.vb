2f001b5882841767931dbb231e84b5be679ed101
MrMilo <legendarylife@hotmail.com>
2014-05-25 23:34:02 +0800

    Added initial database structure
    Renamed Form1.vb to frmLogin.vb, and listen more music.

--- ShopManageSystem/ShopManageSystem/My Project/Application.Designer.vb
+++ ShopManageSystem/ShopManageSystem/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ShopManageSystem.Form1
+            Me.MainForm = Global.ShopManageSystem.frmLogin
         End Sub
     End Class
 End Namespace
